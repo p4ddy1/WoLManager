f6a593c329b9cde3b42319059b682a423c02e405
Patric Kanngießer <mail@lpnw.de>
2018-07-21 13:21:01 +0200

    Implemented auth

--- setup.php
+++ setup.php
@@ -2,7 +2,7 @@
 
 require './vendor/autoload.php';
 
-echo 'Creating tables...';
+echo "Creating tables...\n";
 
 $db = \App\Classes\Database::getInstance();
 
@@ -27,4 +27,22 @@ $query = 'CREATE TABLE IF NOT EXISTS users
 
 $db->exec($query);
 
-echo 'Tables created!';
+echo "Tables created!\n";
+echo "Please add an account\n";
+do {
+    echo "Username: ";
+    $username = trim(fgets(STDIN));
+}while(empty($username));
+
+do {
+    echo "Password: ";
+    $password = trim(fgets(STDIN));
+}while(empty($password));
+
+$user = new \App\Models\User();
+$user->setUsername($username);
+$user->setPassword($password);
+$user->create();
+
+echo "\nUser created!\n";
+echo "We are done!\n";
