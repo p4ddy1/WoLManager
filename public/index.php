85a02a64330b4f9a9655a172a9264f4e1e732bb7
Patric Kanngießer <mail@lpnw.de>
2018-07-24 23:53:57 +0200

    Changed routing

--- public/index.php
+++ public/index.php
@@ -5,24 +5,21 @@ require '../vendor/autoload.php';
 $router = new Bramus\Router\Router();
 $router->setNamespace('\App\Controllers');
 
-$router->before('GET', '/', function() {
-    if(!\App\Classes\Config::getInstance()->isConfigured()){
-        header('Location: /setup');
-    }
-});
-
-$router->get('/', 'IndexController@index');
-$router->get('/devices', 'DeviceController@index');
-$router->get('/devices/add', 'DeviceController@create');
-$router->post('/devices/add', 'DeviceController@save');
-$router->get('/devices/delete/(\d+)', 'DeviceController@delete');
-$router->get('/devices/edit/(\d+)', 'DeviceController@edit');
-$router->post('/devices/edit', 'DeviceController@update');
-$router->get('/wake/(\d+)', 'WakeController@wakeupDevice');
-$router->get('/login', 'LoginController@index');
-$router->post('/login', 'LoginController@login');
-$router->get('/logout', 'LoginController@logout');
-$router->get('/setup', 'SetupController@index');
-$router->post('/setup', 'SetupController@setup');
+if(!\App\Classes\Config::getInstance()->isConfigured()) {
+    $router->get('/', 'SetupController@index');
+    $router->post('/setup', 'SetupController@setup');
+}else{
+    $router->get('/', 'IndexController@index');
+    $router->get('/devices', 'DeviceController@index');
+    $router->get('/devices/add', 'DeviceController@create');
+    $router->post('/devices/add', 'DeviceController@save');
+    $router->get('/devices/delete/(\d+)', 'DeviceController@delete');
+    $router->get('/devices/edit/(\d+)', 'DeviceController@edit');
+    $router->post('/devices/edit', 'DeviceController@update');
+    $router->get('/wake/(\d+)', 'WakeController@wakeupDevice');
+    $router->get('/login', 'LoginController@index');
+    $router->post('/login', 'LoginController@login');
+    $router->get('/logout', 'LoginController@logout');
+}
 
 $router->run();
